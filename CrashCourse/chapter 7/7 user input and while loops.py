ebc494ed136b28b8fec5680a8b58c52cc23d262c
RALaRock <ralarock@hotmail.com>
2021-03-10 11:58:54 -0800

    testing git hub think we have it

--- CrashCourse/chapter 7/7 user input and while loops.py
+++ CrashCourse/chapter 7/7 user input and while loops.py
@@ -94,3 +94,4 @@ print("this is a github test")
 # testing github again after renaming repository
 # closed VSCode and renamed folder for local repository, now testing to see if it's still linked for github updates
 # this change is on the PC
+# github local app had to be told where the repository was because of the name change, but all good after fixing that
